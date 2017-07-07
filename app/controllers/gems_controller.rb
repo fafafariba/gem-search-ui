4509571c3fabc64cafcf408086f55ba8fc9c2dda
Fariba <massah.2@gmail.com>
2017-07-07 15:56:55 -0700

    Pass controller specs'

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -6,7 +6,7 @@ class GemsController < ApplicationController
 	def show
 		@gem = Gems.search(params[:name]).first
 		if @gem
-			render json: [{ name: @gem["name"], info: @gem["info"], dependencies: @gem["dependencies"] }], status: :ok
+			render json: { name: @gem["name"], info: @gem["info"], dependencies: @gem["dependencies"] }, status: :ok
 		else
 			render json: { error: "Gem not found" }, status: :not_found
 		end
--- spec/requests/gems_spec.rb
+++ spec/requests/gems_spec.rb
@@ -8,11 +8,7 @@ RSpec.describe "Gems API", type: :request do
 		"    bcrypt() is a sophisticated and secure hash algorithm designed by The OpenBSD project\n    for hashing passwords. The bcrypt Ruby gem provides a simple wrapper for safely handling\n    passwords.\n"
 	}
 	let(:dependencies) {
-		{ "development":[
-			{"name":"rake-compiler","requirements":"~\u003e 0.9.2"},{"name":"rdoc","requirements":"~\u003e 3.12"},{"name":"rspec","requirements":"\u003e= 3"}
-		],
-		"runtime":[]
-		}
+		{"development"=>[{"name"=>"rake-compiler", "requirements"=>"~> 0.9.2"}, {"name"=>"rdoc", "requirements"=>"~> 3.12"}, {"name"=>"rspec", "requirements"=>">= 3"}], "runtime"=>[]}
 	}
 	let(:invalid_name) { "asdff" }
 
@@ -21,22 +17,17 @@ RSpec.describe "Gems API", type: :request do
 		context "when the name matches a Gem" do 
 			
 			before { get "/gems/#{valid_name}" }
-
-			it "returns only one gem" do 
-				debugger
-				expect(JSON.parse(response.body).size).to be > 1
-			end
 			
 			it "returns Gem name" do
-				expect(JSON.parse(response.body).first["name"]).to match(name)
+				expect(JSON.parse(response.body)["name"]).to match(name)
 			end 
 			
 			it "returns Gem info " do
-				expect(JSON.parse(response.body.first["info"])).to match(info)
+				expect(JSON.parse(response.body)["info"]).to match(info)
 			end
 			
 			it "returns Gem dependencies" do
-				expect(JSON.parse(response.body.first["info"])).to match(dependencies)
+				expect(JSON.parse(response.body)["dependencies"]).to match(dependencies)
 			end
 
 			it "returns status code 200" do

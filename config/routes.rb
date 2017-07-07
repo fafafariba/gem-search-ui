2a5485229d839d1c4ac56099ea801383006af977
Fariba <massah.2@gmail.com>
2017-07-07 14:26:22 -0700

    Add rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
-  get '/favorites' => "static_pages#favorites"
-  resources :gems, only: [:index]
+  # get '/favorites' => "static_pages#favorites"
+
+  defaults format: :json do
+    resources :gems, only: [:index]
+  end
 end
